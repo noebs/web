bf2a89ca3e374612980b492238dbd8e85bb11851
Omar M. Osman <omarosmandev@gmail.com>
2021-07-08 16:04:48 +0200

    refactor mohe-arab service to use transactions logger service

--- src/app/pages/mohe-arab/mohe-arab.component.ts
+++ src/app/pages/mohe-arab/mohe-arab.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionsLogger } from 'app/services/transactions-logger.service';
 
 @Component({
   selector: 'app-mohe-arab',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoheArabComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private transLogger: TransactionsLogger
+  ) { }
 
   ngOnInit() {
   }
