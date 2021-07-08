d2a1475f61e307716c2338fcde4d49c580a3a35f
Omar M. Osman <omarosmandev@gmail.com>
2021-07-08 16:07:03 +0200

    add mohe + mohe-arab transactions

--- src/custom-typings.ts
+++ src/custom-typings.ts
@@ -6,4 +6,4 @@ export interface Service {
     iconStyle?: string
 }
 
-export type TransactionType = "BILL_INQUIRY" | "BILL_PAYMENT" | "ELEC_PAY" | "E-15" | "IPIN" | "BALANCE_INQUIRY" | "CARD_TO_CARD" | "CUSTOMS" | "TOP_UP"
+export type TransactionType = "BILL_INQUIRY" | "BILL_PAYMENT" | "ELEC_PAY" | "E-15" | "IPIN" | "BALANCE_INQUIRY" | "CARD_TO_CARD" | "CUSTOMS" | "TOP_UP" | "MOHE" | "MOHE_ARAB"
